d9c407d3788c6fed256a35cc4f1b85301f99fcd8
Subash Chandran <schandran@axidio.com>
2025-07-05 20:47:42 +0530

    new tool

--- sloot-content.js
+++ sloot-content.js
@@ -58,6 +58,7 @@ const tools = [
       { name: "Full Color Screen", path: "full-color-screen-generator", emoji: "🟩", category: "Color" },
 
       { name: "DVD Screensaver", path: "dvd-screensaver", emoji: "📀", category: "Screen" },
+      { name: "Fake Windows Update Screen", path: "fake-windows-update-screen", emoji: "🪟", category: "Screen" }
   // { name: "YouTube Channel Profile Picture Downloader", path: "youtube-channel-profile-picture-downloader", emoji: "📽️", category: "Social" },
 ];
 
